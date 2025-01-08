ab37f64e1271711186a419021b93930004f4e659
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-01-08 22:30:29 +0100

    refactor: update matrix implementation

--- core/matrix.py
+++ core/matrix.py
@@ -60,3 +60,28 @@ class Matrix(object):
             [0,  0, 1, 0],
             [0,  0, 0, 1]  
         ]).astype(float)
+
+    @staticmethod 
+    def makeScale(s):
+        return numpy.array([
+            [s, 0, 0, 0],
+            [0, s, 0, 0],
+            [0, 0, s, 0],
+            [0, 0, 0, 1],
+        ]).astype(float) 
+    
+    @staticmethod
+    def makePerspective(angleOfView=60, aspectRatio=1, near=0.1, far=1000):
+        a = angleOfView * pi / 180.0
+        d = 1.0 / tan(a/2)
+        r = aspectRatio
+        b = (far + near) / (near - far)
+        c = 2*far*near/ (near - far)
+        
+        return numpy.array([
+            [d/r, 0, 0, 0],
+            [0,   d, 0, 0],
+            [0,   0, b, c],
+            [0, 0, -1, 0]
+        ]).astype(float) 
+    
