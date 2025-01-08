c2a2dc365199fee9243052b437e7dc5ce53db6c2
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-01-08 23:17:20 +0100

    refactor: update acceleration / deceleration test

--- test-2-11-1.py
+++ test-2-11-1.py
@@ -4,6 +4,10 @@ from core.attribute import Attribute
 from core.uniform import Uniform
 from OpenGL.GL import *
 
+import time
+
+from core.util import Util
+
 class Test(Base):
 
     def initialize(self):
@@ -67,41 +71,46 @@ class Test(Base):
         self.baseColors.locateVariable(self.programRef, 'baseColor')
 
         # movement speed
-        self.speed = 2
+        self.speed = 0
+        self.maxSpeed = 2
         self.timeSince = 0
-        self.acceleration = 0.02
-        self.stopping = False
+        self.acceleration = 1.5
+        self.stopping = (0, 0)
+        self.moving = (0, 0) 
 
     def update(self):
 
         x, y, z = self.translations.data
+        
+        distance = 0
 
-        if self.stopping in [-1, 1]:
-            self.timeSince = max(0, self.timeSince - self.deltaTime)
-            distance = self.acceleration * self.timeSince**2
-            x += distance * self.stopping 
-            if self.timeSince <= 0:
-                self.stopping = 0
-        else: 
-            self.timeSince = self.timeSince + self.deltaTime
-            if (self.acceleration * self.timeSince**2 > self.speed * self.deltaTime):
-                distance = self.speed * self.deltaTime
-            else:
-                distance = self.acceleration * self.timeSince**2 
-            
-            
-        if self.input.isKeyPressed("left"):
+        if self.stopping == (0, 0) and self.moving != (0, 0):  
+            self.speed = min(self.maxSpeed, Util.getCurrentSpeed(self.acceleration, 0, time.time_ns(), self.timeSince))
+            distance = self.speed * self.deltaTime 
+        elif self.stopping != (0, 0) and self.moving == (0, 0):  
+            speed = max(0, Util.getCurrentSpeed(-self.acceleration, self.speed, time.time_ns(), self.timeSince))
+            distance = speed * self.deltaTime 
+            if speed == 0:
+                self.stopping = (0,0)
+                self.speed = 0
+                self.timeSince = 0
+        
+
+        if self.input.isKeyPressed("left") or self.stopping == (-1, 0):
             x -= distance
-        if self.input.isKeyPressed("right"):
+        if self.input.isKeyPressed("right") or self.stopping == (1, 0):
             x += distance
-        if self.input.isKeyUp('left'):
-            self.stopping = -1
-            self.timeSince = 0.68
-        if self.input.isKeyUp('right'):
-            self.timeSince = 0.68
-            self.stopping = 1
+            
+        if self.input.isKeyDown('left') or self.input.isKeyDown('right'):
+            self.stopping = (0, 0)
+            self.moving = (-1, 0) if self.input.isKeyDown('left') else (1, 0)
+            self.timeSince = time.time_ns()
+        if self.input.isKeyUp('left') or self.input.isKeyUp('right'):
+            self.moving = (0, 0)
+            self.stopping = (-1, 0) if self.input.isKeyUp('left') else (1, 0)
+            self.timeSince = time.time_ns()
 
-
+        
         if x  >= abs(1 - self.triangleStartXPos):
             x = -(1 - abs(self.triangleStartXPos) + self.triangleWidth)        
         elif x  < -(1 - abs(self.triangleStartXPos) + self.triangleWidth ):
