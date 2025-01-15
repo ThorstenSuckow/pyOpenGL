08172e8a7d8d3cf580f7ec92ae9944ed9e5ef59b
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-01-15 22:39:07 +0100

    refactor: add string ids
    update rendering using regular shaders

--- sandbox/lessons/objectinfo.py
+++ sandbox/lessons/objectinfo.py
@@ -26,8 +26,15 @@ vertexCode = """
 fragmentCode = """
     out vec4 FragColor;
 
+    void main() {
+        FragColor = vec4(1, 0, 0, 1);
+    }
+"""
+
+
+pickingFragmentCode = """
+    out vec4 FragColor;
     
-    uniform vec4 PickingColor2;
     uniform vec4 PickingColor;
 
     void main() {
@@ -83,10 +90,11 @@ io.display_size = size
 
 
 # opengl
+pickingProgramRef = OpenGLUtils.initializeProgram(vertexCode, pickingFragmentCode)
 programRef = OpenGLUtils.initializeProgram(vertexCode, fragmentCode)
-pickingColorUniformLocation = glGetUniformLocation(program=programRef, name="PickingColor"),  
-pickingColorUniformLocation = pickingColorUniformLocation[0] 
 
+pickingColorUniformLocation = glGetUniformLocation(program=pickingProgramRef, name="PickingColor"),  
+pickingColorUniformLocation = pickingColorUniformLocation[0] 
 
 vaoHandles = glGenVertexArrays(2)
 vboHandles = glGenBuffers(2)
@@ -122,6 +130,7 @@ wasClicked = False
 screen_width = screen.get_size()[0]
 screen_height = screen.get_size()[1]
 
+
 while running:
 
     for event in pygame.event.get():
@@ -131,9 +140,8 @@ while running:
             newSize = event.dict['size']
             screen_width = newSize[0]
             screen_height = newSize[1]
-
-
         overlay.process_event(event)
+
     overlay.process_inputs()
   
     point_vertices[0] = x / 100
@@ -143,82 +151,98 @@ while running:
     glClearColor(0, 0, 0, 1)
     glClear(GL_COLOR_BUFFER_BIT)
 
+    # imgui should be rendered.
+    # in this case we switch between pickingFragment-shader and "normal"
+    # fragment shader. we'll utilize imguis mouse event to trigger
+    # identifying objects
+    if render_imgui:
+        mousePos = imgui.get_mouse_pos()    
+        
+        if imgui.is_mouse_clicked(0):
+            wasClicked = mousePos
 
+            glUseProgram(pickingProgramRef)    
+        
+            # point drawing
+            glBindVertexArray(vaoHandles[0])
+            uniformColorId(pickingColorUniformLocation, 1324125)
+            glBindBuffer(GL_ARRAY_BUFFER, vboHandles[0])
+            glBufferData(GL_ARRAY_BUFFER, numpy.array(point_vertices).astype(numpy.float32), GL_STATIC_DRAW)
+            glDrawArrays(GL_POINTS, 0, 1)
+
+            # triangle drawing
+            glBindVertexArray(vaoHandles[1])
+            uniformColorId(pickingColorUniformLocation, 1214111)
+            glBindBuffer(GL_ARRAY_BUFFER, vboHandles[1])
+            glBufferData(GL_ARRAY_BUFFER, numpy.array(triangle_vertices).astype(numpy.float32), GL_STATIC_DRAW)
+            glDrawArrays(GL_TRIANGLE_STRIP, 0, 3)
+
+            glUseProgram(0)
+            glBindVertexArray(0)
+            glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+            glFlush()
+            glFinish()
+            glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+            data = glReadPixels(
+                wasClicked[0], 
+                screen_height - wasClicked[1],
+                1,1,
+                GL_RGBA, GL_UNSIGNED_BYTE)
+
+
+    # switch back to regular program
     glUseProgram(programRef)    
-    
+
     # point drawing
     glBindVertexArray(vaoHandles[0])
-    uniformColorId(pickingColorUniformLocation, 1324125)
     glBindBuffer(GL_ARRAY_BUFFER, vboHandles[0])
     glBufferData(GL_ARRAY_BUFFER, numpy.array(point_vertices).astype(numpy.float32), GL_STATIC_DRAW)
     glDrawArrays(GL_POINTS, 0, 1)
 
     # triangle drawing
     glBindVertexArray(vaoHandles[1])
-    uniformColorId(pickingColorUniformLocation, 1214111)
     glBindBuffer(GL_ARRAY_BUFFER, vboHandles[1])
     glBufferData(GL_ARRAY_BUFFER, numpy.array(triangle_vertices).astype(numpy.float32), GL_STATIC_DRAW)
     glDrawArrays(GL_TRIANGLE_STRIP, 0, 3)
 
-    # reset for imgui
-    if not render_imgui:
-        pygame.display.flip()
-        continue
-
+    
     glUseProgram(0)
     glBindVertexArray(0)
     glBindBuffer(GL_ARRAY_BUFFER, 0)
-    
-    
-    imgui.new_frame()
-    imgui.begin("selecting objects", True)
-    imgui.set_window_size(280, 180)
-    imgui.begin_child("Child 1", border=False, flags=imgui.WINDOW_ALWAYS_AUTO_RESIZE)
 
-    
-    mousePos = imgui.get_mouse_pos()    
-    
-    normal_x, normal_y = normalize(
-        mousePos[0], mousePos[1], 
-        screen_width, screen_height
-    )
 
-    imgui.text(f"Mouse pos (normalized): {normal_x}, {normal_y}")
-
-    if imgui.is_mouse_clicked(0):
-        wasClicked = mousePos
-
+    if render_imgui:
+        imgui.new_frame()
+        imgui.begin("selecting objects", True)
+        imgui.set_window_size(280, 180)
+        imgui.begin_child("Child 1", border=False, flags=imgui.WINDOW_ALWAYS_AUTO_RESIZE)
     
-    
-    imgui.text(f"screen: {screen_width} x {screen_height}")
-            
-    if wasClicked:
-    
-        glFlush()
-        glFinish()
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-        data = glReadPixels(
-            wasClicked[0], 
-            screen_height - wasClicked[1],
-            1,1,
-              GL_RGBA, GL_UNSIGNED_BYTE)
-
         normal_x, normal_y = normalize(
-            wasClicked[0], wasClicked[1], 
+            mousePos[0], mousePos[1], 
             screen_width, screen_height
         )
 
-        imgui.text(f"click x/y: {wasClicked}")
-        imgui.text(f"normalized x/y: ({normal_x}, {normal_y})")
+        imgui.text(f"Mouse pos (normalized): {normal_x}, {normal_y}")
+    
+        imgui.text(f"screen: {screen_width} x {screen_height}")
         
-        imgui.text(f"Selected: {getIdForColor((data[0], data[1], data[2]))}")# r{data[0]/255} g{data[1]/255} b{data[2]/255} a{data[3]/255}")
+        if wasClicked:
+            normal_x, normal_y = normalize(
+                wasClicked[0], wasClicked[1], 
+                screen_width, screen_height
+            )
 
-    imgui.end_child()
-    imgui.end()
-    
-    
-    imgui.render()
-    overlay.render(imgui.get_draw_data())
+            imgui.text(f"click x/y: {wasClicked}")
+            imgui.text(f"normalized x/y: ({normal_x}, {normal_y})")
+            
+            imgui.text(f"Selected: {getIdForColor((data[0], data[1], data[2]))}")# r{data[0]/255} g{data[1]/255} b{data[2]/255} a{data[3]/255}")
+
+        imgui.end_child()
+        imgui.end()
+        
+        imgui.render()
+        overlay.render(imgui.get_draw_data())
 
     pygame.display.flip()
   
