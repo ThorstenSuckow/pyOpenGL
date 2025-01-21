8612ad7eb7bfa562b90692acf25584681651df59
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-01-21 22:50:07 +0100

    feat: add axis rendering

--- sandbox/lessons/directionvectors.py
+++ sandbox/lessons/directionvectors.py
@@ -12,6 +12,25 @@ vertexShader = """
     }
 """
 
+axisVertexShader = """
+    layout (location = 0) in vec3 position;
+    layout (location = 1) in vec3 axisColor;
+    out vec3 axis_color;
+    void main() {
+        axis_color = axisColor;
+        gl_Position = vec4(position, 1);
+    }
+"""
+
+axisFragmentShader = """
+    in vec3 axis_color;
+    out vec4 FracColor;
+    void main() {
+        FracColor = vec4(axis_color, 1);
+    }
+
+"""
+
 fragmentShader = """
     out vec4 FragColor;
 
@@ -25,17 +44,29 @@ size = 512, 512
 pygame.display.set_mode(size, pygame.DOUBLEBUF | pygame.OPENGL)
 
 programRef = OpenGLUtils.initializeProgram(vertexShader, fragmentShader)
+axisProgramRef = OpenGLUtils.initializeProgram(axisVertexShader, axisFragmentShader)
 
 vertices = [
-    -0.25, 0, 0,
-    -0.25, 0.5, 0,
-    0.25, 0, 0,
-    0.25, 0.5, 0
+    #  x      y   z
+    -0.25,    0,  0,
+    -0.25,  0.5,  0,
+     0.25,    0,  0,
+     0.25,  0.5,  0
+]
+
+axis = [
+    0, 0.25, 0,
+    1, 0, 0, 
+    0.5, 0.25, 0, 
+    1, 0, 0,
+    0, 0.25, 0,
+    0, 1, 0,
+    0, 0.75, 0,
+    0, 1, 0
 ]
 
 vaoHandle = glGenVertexArrays(1)
 vboHandle = glGenBuffers(1)
-
 glBindVertexArray(vaoHandle)
 glBindBuffer(GL_ARRAY_BUFFER, vboHandle)
 
@@ -46,13 +77,20 @@ glVertexAttribPointer(
     ctypes.c_void_p(0)
 )
 
-glBufferData(
-    GL_ARRAY_BUFFER,
-    numpy.array(vertices).astype(numpy.float32),
-    GL_STATIC_DRAW     
+vaoHandleAxis = glGenVertexArrays(1)
+glBindVertexArray(vaoHandleAxis)
+glEnableVertexAttribArray(0)
+glVertexAttribPointer(
+    0, 3, GL_FLOAT, GL_FALSE, 
+    6 * sizeof(GLfloat), 
+    ctypes.c_void_p(0)
 )
-
-glUseProgram(programRef)
+glEnableVertexAttribArray(1)
+glVertexAttribPointer(
+    1, 3, GL_FLOAT, GL_FALSE, 
+    6 * sizeof(GLfloat), 
+    ctypes.c_void_p(3 * sizeof(GLfloat))
+)
 
 glEnable(GL_BLEND)
 glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
@@ -66,11 +104,28 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
+    
     glClearColor(0, 0 , 0, 0)
     glClear(GL_COLOR_BUFFER_BIT)
 
-
+    glUseProgram(programRef)
+    glBindVertexArray(vaoHandle)
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        numpy.array(vertices).astype(numpy.float32),
+        GL_STATIC_DRAW     
+    )
     glDrawArrays(GL_TRIANGLE_STRIP, 0, len(vertices) // 3)
+
+    glUseProgram(axisProgramRef)
+    glBindVertexArray(vaoHandleAxis)
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        numpy.array(axis).astype(numpy.float32),
+        GL_STATIC_DRAW     
+    )
+    glDrawArrays(GL_LINES, 0, len(axis) // 3)
+
+
 
     pygame.display.flip()
